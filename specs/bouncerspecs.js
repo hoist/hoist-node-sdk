10620bfeed48b9c45adeb126ccaa7ea06816fbd1
Jamie Wilson <jamie.wilsons@gmail.com>
2015-03-30 11:06:32 -0700

    singleton and callbacks

--- specs/bouncerspecs.js
+++ specs/bouncerspecs.js
@@ -7,18 +7,34 @@ var Hoist = require('../lib');
 
 describe('bouncer tests', function () {
 
-  describe('generateBounceUrl', function() {
-    var hoist;
+  describe('generateBounceUrl with promise', function() {
     var url;
     before(function(done) {
-      hoist = new Hoist();
-      hoist.getBounceUrl({
+      Hoist.getBounceUrl({
         organisation: 'showen8635',
         application: 'dev',
         connector: 'mykey',
         bucketKey: 'bucketKey',
         returnUrl: 'https://hoist.io/return'        
       }).then(function(redirect) {
+        url = redirect.substr(0, 32);
+        done();
+      });
+    });
+    it('should return a url', function() {
+      expect(url).to.eql("https://bouncer.hoist.io/bounce/");
+    });  
+  });
+  describe('generateBounceUrl with callback', function() {
+    var url;
+    before(function(done) {
+      Hoist.getBounceUrl({
+        organisation: 'showen8635',
+        application: 'dev',
+        connector: 'mykey',
+        bucketKey: 'bucketKey',
+        returnUrl: 'https://hoist.io/return'        
+      }, function(redirect) {
         url = redirect.substr(0, 32);
         done();
       });
